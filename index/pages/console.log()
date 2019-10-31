f81e0cd4a2b5d1fb25781d2767c3dd8c5dedc21c
Kristintreitz <55264407+Kristintreitz@users.noreply.github.com>
2019-10-31 18:02:01 -0400

    Add files via upload

---
+++ index/pages/console.log()
@@ -0,0 +1,11 @@
+
+
+
+var word = 'table';
+
+var number = 1666;
+
+Var areYouHungry = true;
+
+
+console.log(word, number, areYouHungry);
---
+++ index/pages/script.js
@@ -0,0 +1,23 @@
+
+
+
+var word = 'table';
+
+var number = 2003;
+
+var current = 2019;
+
+var areYouHungry = true;
+
+var fruits = ['apple', 'banana', 'grapes']
+
+var sum=number+current;
+
+
+// console. log(word, number, areYouHungry, fruits);
+function add(x, y){
+	var result = x+y;
+	return result; };
+
+var answer = add(number, current);
+console.log(answer);
